792a4b438d5186fcca822275553b1b88265c70b7
Pius Aaron <piuschbz@gmail.com>
2024-09-12 09:57:31 +0100

    Add: Session management with JWT

--- pova-backend/controllers/AuthController.js
+++ pova-backend/controllers/AuthController.js
@@ -1,5 +1,7 @@
 import { addUser, getUser } from "../models/user.js";
+import Joi from "joi";
 import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 
 class AuthController{
     static async registerUser(req, res) {
@@ -17,31 +19,43 @@ class AuthController{
     }
 
 
-    static async loginUser(req, res){
-        // extracts credentials
-        const cred = req.body;
+    static async loginUser(req, res) {
+        const loginSchema = Joi.object({
+            email: Joi.string().email().required(),
+            password: Joi.string().min(6).required(),
+        });
 
-        // Create a schema in models to valid login DO NOT FORGET
-        if (!cred){
-            return res.status(400).json({error: "Missing login credentials"});
-        } else if (!'password' in cred){
-            return res.status.json({"error": "Missing password"});
-        } else if (!('email' in cred)){
-            return res.status(400).json({error: "Missing email"});
+        const { error } = loginSchema.validate(req.body);
+        if (error) {
+            return res.status(400).json({ error: error.details[0].message });
         }
 
-        const user = await getUser({email: cred.email});
-        if (!user){
-            return res.status(404).json({error: "User not found"});
-            // compare password hash
-        } else if (!bcrypt.compareSync(cred.password, user.password)){
-            return res.status(401).json({error: "Unauthorized"});
+        const { email, password } = req.body;
+        try {
+            const user = await getUser({ email });
+            if (!user) {
+                return res.status(404).json({ error: "User not found" });
+            }
+
+            const isPasswordValid = bcrypt.compareSync(password, user.password);
+            if (!isPasswordValid) {
+                return res.status(401).json({ error: "Unauthorized" });
+            }
+
+            // clean up the user object before sending the response
+            user.userId = user._id;
+            delete user.password;
+            delete user._id;
+
+            const accessToken = jwt.sign({user: user.id}, process.env.ACCESS_TOKEN_SECRET_KEY,
+                {expiresIn: '2d'});
+            user.accessToken = accessToken;
+            return res.status(200).json(user);
+        } catch (err) {
+            console.error(err);
+            return res.status(500).json({ error: "An error occurred" });
         }
-        user.id = user._id;
-        delete user.password;
-        delete user._id;
-        res.status(200).json(user);
     }
 }
-
 export default AuthController;
+export {jwt};
