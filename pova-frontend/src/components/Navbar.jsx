da8acc8302009d7248e43f09bdf643277c46661f
Anphimens <philipinamensah19@gmail.com>
2024-09-12 10:15:15 +0100

    initial commit, setting up navbar

---
+++ pova-frontend/src/components/Navbar.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const navLinks = [
+  {
+    title: "Homepage",
+    path: "/",
+  },
+  {
+    title: "About",
+    path: "/about",
+  },
+  {
+    title: "Categories",
+    path: "/categories",
+  },
+  {
+    title: "Pages",
+    path: "/pages",
+  },
+  {
+    title: "Contact",
+    path: "/contact us",
+  },
+];
+
+export default function Navigation() {
+  return (
+    <nav className="site-navigation">
+      <span>POVA</span>
+      <ul>
+        {navLinks.map((link, index) => (
+          <li key={index}>
+            <Link to={link.path}>{link.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+}
