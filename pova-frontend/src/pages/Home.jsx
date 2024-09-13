3e946efed7d4c0ee51304b475284e71f9e7b48f7
Anphimens <philipinamensah19@gmail.com>
2024-09-13 17:40:40 +0100

    modified features for home page

--- pova-frontend/src/pages/Home.jsx
+++ pova-frontend/src/pages/Home.jsx
@@ -1,7 +1,25 @@
 import React from "react";
 
 function Home() {
-  return <div>Home page</div>;
+  return(
+    
+  <div className="home-wrapper">
+    {/* parent container for all elements */}
+    <div className="home-container">
+    <span className="popular-post">Popular Post</span> {/* left side */}
+    <span className="popular-post">Popular Post</span> {/* right side */}
+    </div>
+     {/* Separate container for popular videos */}
+     <div className="popular-videos">
+      <p>Video 1</p>
+      <p>Video 2</p>
+      </div>
+
+
+    </div>
+
+
+    )
 }
 
 export default Home;
