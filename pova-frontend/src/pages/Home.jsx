0f0c1b112fa9bd3d396efa83246e64f0096c92eb
Anphimens <philipinamensah19@gmail.com>
2024-09-14 11:45:37 +0100

    two containers created for popular videos

--- pova-frontend/src/pages/Home.jsx
+++ pova-frontend/src/pages/Home.jsx
@@ -6,20 +6,22 @@ function Home() {
   <div className="home-wrapper">
     {/* parent container for all elements */}
     <div className="home-container">
+
+      {/* Left side with popular post and videos */}
+      <div className="left-side">
     <span className="popular-post">Popular Post</span> {/* left side */}
-    <span className="popular-post">Popular Post</span> {/* right side */}
-    </div>
-     {/* Separate container for popular videos */}
-     <div className="popular-videos">
-      <p>Video 1</p>
-      <p>Video 2</p>
+    {/* Separate container for popular videos */}
+    <div className="popular-videos">
+      <p className="video-container">Video 1</p>
+      <p className="video-container">Video 2</p>
+      </div>
       </div>
 
-
+ 
+    <span className="popular-post">Popular Post</span> {/* right side */}
     </div>
-
-
-    )
+    </div>
+    );
 }
 
 export default Home;
